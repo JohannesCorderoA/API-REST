8bcc8e1df39384f3e6749ab559fa2f3472dcf4aa
Johannes Cordero <johannesjaac10@gmail.com>
2021-08-30 14:45:27 -0600

    Commit

---
+++ api/inventario/src/main/java/org/una/inventario/controllers/UsuarioController.java
@@ -0,0 +1,148 @@
+package org.una.inventario.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.una.inventario.dto.*;
+import org.una.inventario.services.IUsuarioService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuarios")
+@Api(tags = {"Usuarios"})
+public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+
+    @GetMapping()
+    @ApiOperation(value = "Obtiene una lista de todos los Usuarios", response = UsuarioDTO.class, responseContainer = "List", tags = "Usuarios")
+    public @ResponseBody
+    ResponseEntity<?> findAll() {
+        try {
+            Optional<List<UsuarioDTO>> result = usuarioService.findAll();
+            if (result.isPresent()) {
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
+        try {
+
+            Optional<UsuarioDTO> usuarioFound = usuarioService.findById(id);
+            if (usuarioFound.isPresent()) {
+                return new ResponseEntity<>(usuarioFound, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/login")
+    @ResponseBody
+    @ApiOperation(value = "Inicio de sesión para conseguir un token de acceso", response = UsuarioDTO.class, tags = "Seguridad")
+    public ResponseEntity<?> login(@PathVariable(value = "cedula") String cedula, @PathVariable(value = "password") String password) {
+        try {
+            UsuarioDTO usuario = new UsuarioDTO();
+            Optional<UsuarioDTO> usuarioFound = usuarioService.login(cedula, password);
+            if (usuarioFound.isPresent()) {
+                return new ResponseEntity<>(usuarioFound, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/cedula/{term}")
+    public ResponseEntity<?> findByCedulaAproximate(@PathVariable(value = "term") String term) {
+        try {
+            Optional<List<UsuarioDTO>> result = usuarioService.findByCedulaAproximate(term);
+            if (result.isPresent()) {
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/nombre/{term}")
+    public ResponseEntity<?> findByNombreCompletoAproximateIgnoreCase(@PathVariable(value = "term") String term) {
+        try {
+            Optional<List<UsuarioDTO>> result = usuarioService.findByNombreCompletoAproximateIgnoreCase(term);
+            if (result.isPresent()) {
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/")
+    @ResponseBody
+    public ResponseEntity<?> create(@RequestBody UsuarioDTO usuarioDTO) {
+        try {
+            Optional<UsuarioDTO> usuarioCreated = usuarioService.create(usuarioDTO);
+            return new ResponseEntity<>(usuarioCreated, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody UsuarioDTO usuarioModified) {
+        try {
+            Optional<UsuarioDTO> usuarioUpdated = usuarioService.update(usuarioModified, id);
+            if (usuarioUpdated.isPresent()) {
+                return new ResponseEntity<>(usuarioUpdated, HttpStatus.OK);
+
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+        try {
+            usuarioService.delete(id);
+            return new ResponseEntity<>("Ok", HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<?> deleteAll() throws Exception {
+//        //TODO: Implementar este método
+        throw new Exception("Not implemented Function");
+
+    }
+
+}
